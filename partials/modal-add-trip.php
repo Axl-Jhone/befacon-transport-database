0aa9471c5130bf53a872a7d0fb0101455d605b8d
Dizzy <144509150+Axl-Jhone@users.noreply.github.com>
2025-11-13 00:23:21 +0800

    feat(add-button): add JS functionality and style for the add trip button

---
+++ partials/modal-add-trip.php
@@ -0,0 +1,7 @@
+<div class="modal-container" id="modal_wrapper">
+    <div class="modal">
+        <h1>Add Trip</h1>
+        <button class="action-button" id="close">Cancel</button>
+        <button class="action-button">Add</button>
+    </div>
+</div>
