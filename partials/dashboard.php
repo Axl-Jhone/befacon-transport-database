bae5966b398a4218498e661294b3ecff8e9e116d
Magsuwellan <factora63@gmail.com>
2025-11-13 09:00:39 +0800

    feat(dashboard): Added a welcome page

--- partials/dashboard.php
+++ partials/dashboard.php
@@ -1,1 +1,4 @@
-<div class="content">Under Construction</div>
+<div class="table-content dashboard-welcome">
+    <h1>Welcome to BeFaCon Transport Database Dashboard</h1>
+    <p>Select a section from the sidebar to get started.</p>
+</div>
