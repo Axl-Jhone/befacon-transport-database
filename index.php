d2d82cc4ff1f4047b2ad4f2161c47ee62cfd9ec6
Dizzy <144509150+Axl-Jhone@users.noreply.github.com>
2025-11-18 00:58:25 +0800

    ui(navbar): add and style navbar

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GoBeFaCon</title>
+    <link rel="stylesheet" href="/befacon-transport-database/assets/css/navbar.css">
+    <link rel="stylesheet" href="/befacon-transport-database/assets/css/main.css">
+</head>
+<body>
+    <?php include ('components/navbar.php') ?>
+    <h1>Hello</h1>
+    
+</body>
+</html>
