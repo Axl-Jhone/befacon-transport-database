8c88f6a4347a972f2073c0d2374822059156bece
Dizzy <144509150+Axl-Jhone@users.noreply.github.com>
2025-11-18 16:04:23 +0800

    ui(sidebar): add and style sidebar

---
+++ components/sidebar.php
@@ -0,0 +1,97 @@
+<aside class="sidebar">
+    <p class="section-title">MENU</p>
+    <ul class="main-menu">
+        <li>
+            <a class="menu-link" href="#">
+                <i class="fa-solid fa-chart-pie"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+
+        <li>
+            <a class="menu-link" href="#">
+                <i class="fa-solid fa-route"></i>
+                <span>Trips</span>
+            </a>
+        </li>
+
+        <li>
+            <a class="menu-link" href="#">
+                <i class="fa-solid fa-users"></i>
+                <span>Drivers</span>
+            </a>
+        </li>
+
+        <li>
+            <a class="menu-link" href="#">
+                <i class="fa-solid fa-taxi"></i>
+                <span>Vehicles</span>
+            </a>
+        </li>
+
+        <li class="has-submenu">
+            <a class="menu-link" href="#">
+                <i class="fa-solid fa-table"></i>
+                <span>Lookup Tables</span>
+                <i class="fa-solid fa-angle-right dropdown"></i>
+            </a>
+
+            <ul class="submenu">
+                <li>
+                    <a class="submenu-link" href="#">
+                        <span>Access Type</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a class="submenu-link" href="#">
+                        <span>Driver Status</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a class="submenu-link" href="#">
+                        <span>License Type</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a class="submenu-link" href="#">
+                        <span>Purpose Type</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a class="submenu-link" href="#">
+                        <span>Trip Status</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a class="submenu-link" href="#">
+                        <span>Vehicle Condition</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a class="submenu-link" href="#">
+                        <span>Vehicle Status</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a class="submenu-link" href="#">
+                        <span>Vehicle Type</span>
+                    </a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+
+    <div class="bottom">
+        <p class="section-title">OTHERS</p>
+        <ul class="main-menu">
+            <li><a class="menu-link" href="#"><i class="fa-solid fa-circle-info"></i><span>About us</span></a></li>
+        </ul>
+    </div>
+</aside>
--- index.php
+++ index.php
@@ -5,11 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GoBeFaCon</title>
     <link rel="stylesheet" href="/befacon-transport-database/assets/css/navbar.css">
+    <link rel="stylesheet" href="/befacon-transport-database/assets/css/sidebar.css">
     <link rel="stylesheet" href="/befacon-transport-database/assets/css/main.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=circle" />
+    <script defer src="https://kit.fontawesome.com/da8e65aaa6.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <?php include ('components/navbar.php') ?>
-    <h1>Hello</h1>
     
+    <div class="main-container">
+        <?php include ('components/sidebar.php') ?>
+    </div>
 </body>
 </html>
