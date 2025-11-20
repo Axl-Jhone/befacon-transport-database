fd77840c7699a40457f0c6ee57ad668e66f104f8
Magsuwellan <factora63@gmail.com>
2025-11-21 04:55:24 +0800

    feat(filter-indicator): Added an indicator for the amount of filter applied to a filter search

--- pages/admin-view/trips.php
+++ pages/admin-view/trips.php
@@ -13,6 +13,16 @@
     $filter_destination = isset($_GET['filter_destination']) ? trim($_GET['filter_destination']) : '';
     $filter_departure = isset($_GET['filter_departure']) ? trim($_GET['filter_departure']) : '';
     $filter_arrival = isset($_GET['filter_arrival']) ? trim($_GET['filter_arrival']) : '';
+    // Count active modal/extended filters for UI indicator
+    $activeFilters = 0;
+    if ($filter_driver_id) $activeFilters++;
+    if ($filter_vehicle_type_id) $activeFilters++;
+    if ($filter_trip_status_id) $activeFilters++;
+    if ($filter_purpose_id) $activeFilters++;
+    if ($filter_origin !== '') $activeFilters++;
+    if ($filter_destination !== '') $activeFilters++;
+    if ($filter_departure !== '') $activeFilters++;
+    if ($filter_arrival !== '') $activeFilters++;
     if ($curr_page < 1) $curr_page = 1;
     if ($curr_page < 1) $curr_page = 1;
     $offset = ($curr_page - 1) * $limit;
@@ -155,6 +165,9 @@
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#183a59" width="24" height="24" onclick="openModal('filter-search-template', 'Filter Trips')">
                 <path d="M440-160q-17 0-28.5-11.5T400-200v-240L168-736q-15-20-4.5-42t36.5-22h560q26 0 36.5 22t-4.5 42L560-440v240q0 17-11.5 28.5T520-160h-80Zm40-308 198-252H282l198 252Zm0 0Z"/>
             </svg>
+            <?php if (!empty($activeFilters)): ?>
+                <span class="filter-indicator"><?php echo $activeFilters; ?></span>
+            <?php endif; ?>
         </button>
 
         <form id="searchForm" method="GET" action="">
